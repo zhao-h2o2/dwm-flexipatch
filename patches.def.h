96f3c718f196a76cfca21431d27310554b454adf
Zhaohui Wang <18811365389@163.com>
2024-08-10 22:55:43 +0800

    enable ipc patch for polybar

--- patches.def.h
+++ patches.def.h
@@ -741,7 +741,7 @@
  * https://github.com/mihirlad55/dwm-ipc
  * https://dwm.suckless.org/patches/ipc/
  */
-#define IPC_PATCH 0
+#define IPC_PATCH 1
 
 /* Adds rule option for clients to avoid accidental termination by killclient for sticky windows.
  * https://dwm.suckless.org/patches/ispermanent/
